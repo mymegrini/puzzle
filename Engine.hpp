f70bfb688163ad2f256eddce28af50e72840572e
soheib <soheib.biga@outlook.fr>
2016-12-31 23:36:29 +0100

    Moving in a azerty board :p

--- Engine.hpp
+++ Engine.hpp
@@ -14,65 +14,69 @@ class Engine {
 
 public:
   Engine(int n, int m, bool human=true):human(human){
-	if (n*m == 0)
-	  g = G();
-	else
-	  g = G(n,m);
+    if (n*m == 0)
+      g = G();
+    else
+      g = G(n,m);
   }
 
   virtual void Run(){
 
-	g.Init();
-	g.Print();
-	std::this_thread::sleep_for(2s);
-	g.Start();
-	g.Print();
-	while(!g.GameOver()){
-	  g.Play(Player());
-	  g.Print();
-	}
+    g.Init();
+    g.Print();
+    std::this_thread::sleep_for(2s);
+    g.Start();
+    g.Print();
+    while(!g.GameOver()){
+      g.Play(Player());
+      g.Print();
+    }
   }
 
   virtual Input Player(){
 
-	if(human){
-	  string input;
-	  cin >> input;
-	  switch(input[0]){
-	  case 'w' :
-		return Input::UP;
-	  case 'a' :
-		return Input::LEFT;
-	  case 's' :
-		return Input::DOWN;
-	  case 'd' :
-		return Input::RIGHT;
-	  case 'c' :
-		exit(0);
-	  default :
-		return Input::UNHANDLED;
-	  }
-	}
-	else
-	  return AutoPlayer();
+    if(human){
+      string input;
+      cin >> input;
+      switch(input[0]){
+      case 'w' :
+	return Input::UP;
+      case 'z' :
+	return Input::UP;
+      case 'a' :
+	return Input::LEFT;
+      case 'q' :
+	return Input::LEFT;
+      case 's' :
+	return Input::DOWN;
+      case 'd' :
+	return Input::RIGHT;
+      case 'c' :
+	exit(0);
+      default :
+	return Input::UNHANDLED;
+      }
+    }
+    else
+      return AutoPlayer();
   }
 
 protected:
   Input AutoPlayer(){
 	
-	std::this_thread::sleep_for(1s);
-	switch(rand() % 4){
-	case 0 :
-	  return Input::UP;
-	case 1 :
-	  return Input::LEFT;
-	case 2 :
-	  return Input::DOWN;
-	case 3 :
-	  return Input::RIGHT;
-	default :
-	  return Input::UNHANDLED;
-	}
+    std::this_thread::sleep_for(1s);
+    switch(rand() % 4){
+    case 0 :
+      return Input::UP;
+    case 1 :
+      return Input::LEFT;
+    case 2 :
+      return Input::DOWN;
+    case 3 :
+      return Input::RIGHT;
+    default :
+      return Input::UNHANDLED;
+    }
   }
   bool human;
   G g;
