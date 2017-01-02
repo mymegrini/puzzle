941d56fed5d7f0507b67752445a5697000883123
Younes Megrini <rasetoi@yahoo.fr>
2017-01-02 12:18:40 +0100

    2035: virtual inheritance

--- Grid2035.hpp
+++ Grid2035.hpp
@@ -3,10 +3,10 @@
 
 #include "Grid2048.hpp"
 
-class Grid2035 : public Grid2048{
+class Grid2035 : virtual public Grid2048{
 
 public:
-  Grid2035(int i=4, int j=4):Grid2048(i,j,"2048v35"){}
+  Grid2035(int i=4, int j=4):Grid2048(i,j,"2035"){}
   virtual int Rand() override;
 };
 #endif
