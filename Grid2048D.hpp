5e928a29c6228792164d85fd354f5496588b69d2
Younes Megrini <rasetoi@yahoo.fr>
2017-01-02 12:20:21 +0100

    2048D: virtual inheritance

--- Grid2048D.hpp
+++ Grid2048D.hpp
@@ -3,10 +3,10 @@
 
 #include "Grid2048.hpp"
 
-class Grid2048D : public Grid2048{
+class Grid2048D : virtual public Grid2048{
 
 public:
-  Grid2048D(int i=4, int j=4,const char* name="2048D"):Grid2048(i,j,name){}
+  Grid2048D(int i=4, int j=4, const char* name="2048D"):Grid2048(i,j,name){}
   virtual void Play(Input user) override;
   virtual void Newbox() override;
 };
